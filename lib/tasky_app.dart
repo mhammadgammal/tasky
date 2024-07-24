9ff8bee2e78e7593b2cc1fc5fc792d9910f36d0d
mhammadgammal <mhammadgamal17@gmail.com>
2024-07-24 10:31:39 +0300

    Init Commit

---
+++ lib/tasky_app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class TaskyApp extends StatelessWidget {
+  const TaskyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Tasky',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: Placeholder(),
+    );
+  }
+}
