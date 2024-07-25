763f7f683519efbedaba9315268852736a8190a2
mhammadgammal <mhammadgamal17@gmail.com>
2024-07-25 10:47:01 +0300

    add password field

--- lib/features/authentication/data/data_source/register_dto.dart
+++ lib/features/authentication/data/data_source/register_dto.dart
@@ -1,23 +1,21 @@
 class RegisterDto {
   late String name;
   late String yearsOfExperience;
-  late ExperienceLevel level;
+  late String level;
   late String address;
   late String phoneNumber;
+  late String password;
 
   RegisterDto(
       {required this.name,
       required this.yearsOfExperience,
       required this.level,
       required this.address,
-      required this.phoneNumber});
+      required this.phoneNumber,
+      required this.password});
 
   RegisterDto.fromJson(Map<String, dynamic> json) {
     name = json['displayName'];
-    yearsOfExperience = json['experienceYears'];
-    level = json['level'];
-    address = json['address'];
-    phoneNumber = json['phone'];
   }
 
   Map<String, dynamic> toJson() {
@@ -27,6 +25,7 @@ class RegisterDto {
       'level': level,
       'address': address,
       'phone': phoneNumber,
+      'password': password
     };
   }
 }
