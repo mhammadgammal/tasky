5acc168e771646f8a7286b189eac611a52754629
mhammadgammal <mhammadgamal17@gmail.com>
2024-07-27 10:36:25 +0300

    remove edit task feature

--- lib/features/tasks/presentation/screens/edit_task/cubit/edit_task_cubit.dart
+++
@@ -1,8 +0,0 @@
-import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
-
-part 'edit_task_state.dart';
-
-class EditTaskCubit extends Cubit<EditTaskState> {
-  EditTaskCubit() : super(EditTaskInitial());
-}
--- lib/features/tasks/presentation/screens/edit_task/cubit/edit_task_state.dart
+++
@@ -1,6 +0,0 @@
-part of 'edit_task_cubit.dart';
-
-@immutable
-sealed class EditTaskState {}
-
-final class EditTaskInitial extends EditTaskState {}
