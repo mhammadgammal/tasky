bab1615b42ee717e7c8c240e47f53f0d216f976e
mhammadgammal <mhammadgamal17@gmail.com>
2024-08-04 14:01:38 +0300

    refactor thumbnail widget to get image remotely not from local storage

--- lib/features/tasks/presentation/widgets/task_thumbnail.dart
+++ lib/features/tasks/presentation/widgets/task_thumbnail.dart
@@ -1,29 +1,44 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
+import 'package:tasky/core/utils/api_utils/api_end_points.dart';
 
 class TaskThumbnail extends StatelessWidget {
   const TaskThumbnail(
       {super.key,
       required this.ifImageExist,
       required this.imagePath,
-      this.thumbnailSize = 50.0});
+      this.thumbnailSize});
 
   final bool ifImageExist;
   final String imagePath;
-  final double thumbnailSize;
+  final double? thumbnailSize;
+
   @override
   Widget build(BuildContext context) {
-    return ifImageExist
-        ? Image.file(
-            File(imagePath),
-            width: thumbnailSize,
-            height: thumbnailSize,
-            fit: BoxFit.cover,
-          )
-        : Icon(
-            Icons.task,
-            size: thumbnailSize,
-          );
+    return Image.network('${ApiEndPoints.baseUrl}images/$imagePath',
+        width: thumbnailSize ?? 50.0,
+        height: thumbnailSize ?? 56.0, loadingBuilder: (BuildContext context,
+            Widget child, ImageChunkEvent? loadingProgress) {
+      if (loadingProgress == null) {
+        return child;
+      } else {
+        return CircularProgressIndicator(
+          value: loadingProgress.expectedTotalBytes != null
+              ? loadingProgress.cumulativeBytesLoaded /
+                  loadingProgress.expectedTotalBytes!
+              : null,
+        );
+      }
+    }, errorBuilder:
+            (BuildContext context, Object error, StackTrace? stackTrace) {
+      return const SizedBox(
+        width: 50.0,
+        height: 56.0,
+        child: Icon(
+          size: 50.0,
+          Icons.error,
+          color: Colors.red,
+        ),
+      );
+    });
   }
 }
