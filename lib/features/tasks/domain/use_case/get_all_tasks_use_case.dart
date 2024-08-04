fa2e97725083e08513d1eb6dbd11b8e1b6555c4f
mhammadgammal <mhammadgamal17@gmail.com>
2024-08-04 13:59:33 +0300

    remove checking if task's image exist locally

--- lib/features/tasks/domain/use_case/get_all_tasks_use_case.dart
+++ lib/features/tasks/domain/use_case/get_all_tasks_use_case.dart
@@ -1,6 +1,5 @@
 import 'package:dartz/dartz.dart';
 import 'package:tasky/features/tasks/data/repository/tasks_repository_impl.dart';
-import 'dart:io';
 
 import '../../../../core/base_use_case/no_parameter_use_case.dart';
 import '../entity/task_model.dart';
@@ -18,11 +17,6 @@ class GetAllTasksUseCase
     if (apiResponse.response != null) {
       for (var task in apiResponse.response!.data) {
         var taskModel = TaskModel.fromJson(task);
-        if (await File(taskModel.imagePath).exists()) {
-          taskModel.isImageExist = true;
-        } else {
-          taskModel.isImageExist = false;
-        }
         tasks.add(taskModel);
       }
       return Left(tasks);
