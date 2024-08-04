9b4ae24b287ac17e19f6de06de9e60647926e426
mhammadgammal <mhammadgamal17@gmail.com>
2024-08-04 12:04:30 +0300

    handle 413 status code

--- lib/core/utils/api_utils/api_error_handler.dart
+++ lib/core/utils/api_utils/api_error_handler.dart
@@ -4,6 +4,8 @@ abstract class ApiErrorHandler {
       case 401: return "Unauthorized";
       case 403: return "Refresh Failed";
       case 501: return "An Error Occurred, Please try again";
+      case 413:
+        return 'Photo is too big, must be less than 1 MB';
       default: return 'UnExpected error';
     }
   }
