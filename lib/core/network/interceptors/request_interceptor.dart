c2d97906a5fa7cc2c8d65e0e878692c10751e318
mhammadgamal <mohamed.gamal@dafagate.com>
2026-07-25 13:56:29 +0300

    refactor: improve token handling and timeout settings in RequestInterceptor

--- lib/core/network/interceptors/request_interceptor.dart
+++ lib/core/network/interceptors/request_interceptor.dart
@@ -4,9 +4,22 @@ class RequestInterceptor extends Interceptor {
   @override
   Future<void> onRequest(
       RequestOptions options, RequestInterceptorHandler handler) async {
-    final token = sl<CacheHelper>().getString(key: CacheKeys.token) ?? '';
-    options.headers['Authorization'] = 'Bearer $token';
+    final token = sl<CacheHelper>().getString(key: CacheKeys.token);
+
+    if (token != null) {
+      options.headers['Authorization'] = 'Bearer $token';
+    }
     options.headers.putIfAbsent('Content-Type', () => 'application/json');
+    // Set the Accept-Language header
+    options.headers['accept-language'] = 'en';
+
+    // Set timeout and error handling behavior
+    options.receiveDataWhenStatusError = true;
+    options.connectTimeout = const Duration(seconds: 30);
+    options.sendTimeout = const Duration(seconds: 30);
+    options.receiveTimeout = const Duration(seconds: 30);
+
+    // Call the next interceptor
     return super.onRequest(options, handler);
   }
 }
